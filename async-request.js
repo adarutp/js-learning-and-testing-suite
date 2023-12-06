6ad2f7aedacd8940b10f24d28044fe99273b5b51
Anton <122996927+adarutp@users.noreply.github.com>
2023-12-06 17:46:02 +0300

    Create async-request.js

---
+++ async-request.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Async Data Fetching</title>
+</head>
+<body>
+    <button onclick="fetchData()">Fetch Data</button>
+    <script>
+        async function fetchData() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+            const data = await response.json();
+            alert('Data fetched: ' + JSON.stringify(data));
+        }
+    </script>
+</body>
+</html>
